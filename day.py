2c71cd10c6ad6bd51c60cbddb4800519fbd4a2a2
PRAJAPATI ARYANKUMAR PRAKASHBHAI <prajapatiaryankumar964@gmail.com>
2026-06-04 18:01:01 +0530

    day choise

---
+++ day.py
@@ -0,0 +1,16 @@
+day=int(input("Enter the number of day:"))
+
+if day==1:
+    print("monday")
+elif day==2:
+    print("teusday")
+elif day==3:
+    print("wednesday")
+elif day==4:
+    print("thursday")
+elif day==5:
+    print("friday")
+elif day==6:
+    print("saturday")
+elif day==7:
+    print("sunday")
