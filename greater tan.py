cdde385c1f1d14152a19eaabcfabd734ee6c8c7e
PRAJAPATI ARYANKUMAR PRAKASHBHAI <prajapatiaryankumar964@gmail.com>
2026-06-11 05:32:46 +0530

    greater than value pro.

---
+++ greater tan.py
@@ -0,0 +1,8 @@
+a=int(input("enter the value for a="))
+b=int(input("enter the value for b="))
+if(a>b):
+    print("a is greater")
+elif(b>a):
+    print("b is greater")
+elif(b):
+    print("both are equal")
